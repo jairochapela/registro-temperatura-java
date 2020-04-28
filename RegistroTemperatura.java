938d1ff5e87659b7136a9506d54bcf1b34c27c8a
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-04-28 17:55:00 +0200

    Cálculos.

--- RegistroTemperatura.java
+++ RegistroTemperatura.java
@@ -8,10 +8,21 @@ class RegistroTemperatura {
 
     private static Map<Date, Double> medidas = new HashMap<>();
 
-    private static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/YYYY");
-    
-    private static double calcularTemperaturaMedia() {
-        return 0.0; // TODO: implementar cálculo.
+    private static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+    private static double calcularTemperaturaMedia(Map<Date, Double> medidas) {
+        double tmedia = 0.0;
+        int numMuestras = 0;
+
+        for (Double temp : medidas.values()) {
+            tmedia += temp;
+            numMuestras++;
+        }
+        if (numMuestras > 0) {
+            return tmedia / numMuestras;
+        } else {
+            return 0;
+        }
     }
 
     public static void main(String[] args) {
@@ -21,6 +32,7 @@ class RegistroTemperatura {
             try {
                 Date fecha = formatter.parse(s.next());
                 double temperatura = s.nextDouble();
+                System.out.println("Dato añadido: " + fecha + " -> " + temperatura);
                 medidas.put(fecha, temperatura);
             } catch (Exception e) {
                 System.out.println("Dato no válido.");
@@ -28,13 +40,13 @@ class RegistroTemperatura {
         }
         s.close();
 
-        System.out.println("Datos introducidos");
+        System.out.println("Datos introducidos: " + medidas.size());
 
         for (Date d : medidas.keySet()) {
             System.out.println(d + " -> " + medidas.get(d));
         }
 
-        System.out.println("Temperatura media: " + calcularTemperaturaMedia() + "ºC");
+        System.out.println("Temperatura media: " + calcularTemperaturaMedia(medidas) + "ºC");
 
     }
 }
